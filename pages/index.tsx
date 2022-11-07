50a6045db7d51184fb54a83bd60bb7fe1ef9ba7b
grapeJUICE1 <anonymyty123@gmail.com>
2022-11-07 13:30:10 +0600

    modularize index.ts in pages folder by making hero component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,7 @@
-import {
-  VStack,
-  Heading,
-  Flex,
-  Center,
-  Button,
-  HStack,
-  Text,
-} from '@chakra-ui/react'
-import Link from 'next/link'
-import Grape from '../icons/Grape'
+import Hero from '../components/Hero'
 
 function Home() {
-  return (
-    <Flex
-      width={'100vw'}
-      height={'90vh'}
-      alignContent={'center'}
-      justifyContent={'center'}
-    >
-      <Center>
-        <VStack>
-          <Heading>
-            <HStack>
-              <Grape width='2.5rem' height='2.5rem' />
-              <Text>Grape Thoughts</Text>
-              <Grape width='2.5rem' height='2.5rem' />
-            </HStack>
-          </Heading>
-          <Link href='/thoughts'>
-            <Button color='purple.600'>Browse All Thougths</Button>
-          </Link>
-        </VStack>
-      </Center>
-    </Flex>
-  )
+  return <Hero />
 }
 
 export default Home
