0415ab1178ce3c3503daec65a6eb253a896b8f76
grapeJUICE1 <anonymyty123@gmail.com>
2022-11-08 00:08:12 +0600

    setup prisma

---
+++ lib/prisma.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client'
+let prisma
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
+}
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
+}
+
+export default prisma
